8d5a0fd1aecae55fe2e194a1eb0e6fa9ecaa01eb
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-08-30 16:50:04 +0100

    Aula 3-02 'Acessando o Banco de Dados'

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 module.exports = app => {
   app.get('/', (req, resp) => {
     resp.send(`
@@ -13,19 +15,13 @@ module.exports = app => {
   });
 
   app.get('/livros', (req, resp) => {
-    resp.marko(
-      require('../views/livros/lista/lista.marko'),
-      {
-        livros: [
-          {
-            id: 1,
-            titulo: 'Fundamentos do Node'
-          },{
-            id: 2,
-            titulo: 'Node Avançado'
-          }
-        ]
-      }
-    );
+    db.all('SELECT * FROM livros', (err, result) => {
+      resp.marko(
+        require('../views/livros/lista/lista.marko'),
+        {
+          livros: result
+        }
+      );
+    });
   });
 };
