0e39278a6ef373aa9ee40a68ef1fe1345e357091
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-08-30 13:29:00 +0100

    Commit inicial e Aula 2-03 'Express em Ação'

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log(`Servidor rodando na porta 3000`);
+});
+
+app.get('/', (req, resp) => {
+  resp.send(`
+    <html>
+      <head>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Casa do Código</h1>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/livros', (req, resp) => {
+  resp.send(`
+    <html>
+      <head>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Listagem de Livros</h1>
+      </body>
+    </html>
+  `);
+});
+/*
+const http = require('http');
+const servidor = http.createServer((req, resp) => {
+  let html = '';
+  if(req.url === '/') {
+    html = `
+    <html>
+      <head>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Casa do Código</h1>
+      </body>
+    </html>
+    `;
+  } else if(req.url === '/livros') {
+    html = `
+      <html>
+        <head>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1>Livros</h1>
+        </body>
+      </html>
+    `
+  }
+  resp.end(html);
+});
+servidor.listen(3000);*/
